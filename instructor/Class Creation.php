be69e35d2fea87243e19dca2c13490ba20b16c1f
Jon Linden <jon8linden@gmail.com>
2011-10-26 16:41:23 -0400

    Added forms

---
+++ instructor/Class Creation.php
@@ -0,0 +1,26 @@
+<!-- Class Creation created by Jon Linden 
+	This will access the class table and student table to get a list of students
+		
+
+-->
+<html>
+	<head></head>
+	<body>
+		Rate Your Mate - Class Creation
+		<title> Class Creation | Rate Your Mate </title>
+		<form name = "className" action = "link" method = "get"> 
+			<!-- input -->
+			Class Name: <input type="text">
+			
+			<!--add class button
+				a warning pop up should be integrated in case a user already exists-->
+			<input type = "submit" value = "Add Class" />
+			
+			<!-- Java script that allows you to drag and drop students from a list into a class 
+				Class list will be on left and student list will be on right-->
+			
+			
+			
+		</form>
+	</body>
+</html>
